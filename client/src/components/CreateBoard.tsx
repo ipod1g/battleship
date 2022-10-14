f30a4b87976c141da9709b3dd9146db171ba45cd
ipod1g <kubonkwan99@gmail.com>
2022-10-15 00:11:56 +0800

    Add component to create empty grid

---
+++ client/src/components/CreateBoard.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { ICreateBoardProps } from '../interfaces/types';
+
+//Creates the board for placement and for the opponent
+const CreateBoard = (props: ICreateBoardProps) => {
+  const { setSquares, width } = props;
+
+  const setSquaresDefined = (array: any[]) => {
+    props.setSquares?.(array);
+  };
+
+  const squaresArray = Array.from({ length: width * width }, (_, i) => (
+    <div key={i} id={`${i}`}>
+      🛥️
+    </div>
+  ));
+
+  useEffect(() => {
+    setSquaresDefined(squaresArray);
+
+    return () => {};
+  }, []);
+
+  return <>{squaresArray}</>;
+};
+
+export default CreateBoard;
