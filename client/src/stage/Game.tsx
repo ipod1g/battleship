c1895535a74a75d13975e07011cbbccdb77fb688
ipod1g <kubonkwan99@gmail.com>
2022-10-17 00:15:02 +0800

    Add stage to render boardgrid & defined ship arrays

---
+++ client/src/stage/Game.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect, useRef } from 'react';
+import CreateBoard from '../components/CreateBoard';
+import GenerateShips from '../components/GenerateShips';
+// import useGenerateShips from '../hooks/GenerateShips';
+
+interface IGame {
+  boardArray: any[];
+  shipArray: { name: string; directions: number[][] }[];
+}
+
+//Conditions 'Sunk' | 'Hit' | 'Miss' | 'Ship' | 'Empty'
+
+const Game = () => {
+  const [userSquares, setUserSquares] = useState<IGame['boardArray']>([]);
+  const [opponentSquares, setOpponentSquares] = useState<IGame['boardArray']>([]);
+  const opponentRef = useRef<HTMLDivElement>(null);
+
+  const width = 10;
+  const [shipArray, setShipArray] = useState<IGame['shipArray']>([
+    {
+      name: 'destroyer',
+      directions: [
+        [0, 1],
+        [0, width],
+      ],
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: 'battleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width * 2, width * 3],
+      ],
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width * 2, width * 3, width * 4],
+      ],
+    },
+  ]);
+
+  //
+  function handleStart() {
+    GenerateShips(opponentSquares, shipArray[0], opponentRef);
+    GenerateShips(opponentSquares, shipArray[1], opponentRef);
+    GenerateShips(opponentSquares, shipArray[2], opponentRef);
+    GenerateShips(opponentSquares, shipArray[3], opponentRef);
+    GenerateShips(opponentSquares, shipArray[4], opponentRef);
+  }
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <div className="battleship-grid grid-user">
+          <CreateBoard width={10} setSquares={setUserSquares}></CreateBoard>
+        </div>
+        <div className="battleship-grid grid-computer" ref={opponentRef}>
+          <CreateBoard width={10} setSquares={setOpponentSquares}></CreateBoard>
+        </div>
+      </div>
+      <button style={{ height: '100px', width: '100px' }} onClick={() => handleStart()}>
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default Game;
