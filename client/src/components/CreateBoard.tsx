e194472fe460dd784fb20a3574094641729e7ec4
ipod1g <kubonkwan99@gmail.com>
2022-10-17 00:14:01 +0800

    Update return div's content

--- client/src/components/CreateBoard.tsx
+++ client/src/components/CreateBoard.tsx
@@ -3,21 +3,16 @@ import { ICreateBoardProps } from '../interfaces/types';
 
 //Creates the board for placement and for the opponent
 const CreateBoard = (props: ICreateBoardProps) => {
-  const { setSquares, width } = props;
+  const { width } = props;
 
   const setSquaresDefined = (array: any[]) => {
     props.setSquares?.(array);
   };
 
-  const squaresArray = Array.from({ length: width * width }, (_, i) => (
-    <div key={i} id={`${i}`}>
-      🛥️
-    </div>
-  ));
+  const squaresArray = Array.from({ length: width * width }, (_, i) => <div key={i} id={`${i}`}></div>);
 
   useEffect(() => {
     setSquaresDefined(squaresArray);
-
     return () => {};
   }, []);
 
