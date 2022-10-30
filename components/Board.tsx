97d71b89a6ed693c7e012193726a6ea9d8a5dcf4
ipod1g <kubonkwan99@gmail.com>
2022-10-31 00:05:54 +0800

    Add drag to place ships functionality

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,17 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export interface Board {
   board: number[][];
+  orientation: string;
+  isShipSelected: boolean;
+  setCurrentGridXLocation: React.Dispatch<
+    React.SetStateAction<number | undefined>
+  >;
+  setCurrentGridYLocation: React.Dispatch<
+    React.SetStateAction<number | undefined>
+  >;
 }
 
-//Creates the board for placement and for the opponent
+/** Creates the board for placement and for the opponent */
 const Board = (props: Board) => {
   return (
     <>
       {props.board.map((column, i) => (
-        <div key={i} id={`column-${i}`}>
+        <div
+          key={i}
+          id={`column-${i}`}
+          onMouseOverCapture={() => {
+            if (!props.isShipSelected) return;
+            else props.setCurrentGridXLocation(i);
+          }}
+        >
           {column.map((value, j) => (
-            <div key={j} id={`row-${j}`}>
+            <div
+              key={j}
+              id={`row-${j}`}
+              onMouseOverCapture={() => {
+                if (!props.isShipSelected) return;
+                else props.setCurrentGridYLocation(j);
+              }}
+            >
               {value}
             </div>
           ))}
