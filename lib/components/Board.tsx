1a7902223837c977360c5930c280719a24c0d486
SimHoZebs <simhozebs@gmail.com>
2022-11-07 10:05:23 -0500

    move placeShip logic from index to Board component

--- lib/components/Board.tsx
+++ lib/components/Board.tsx
@@ -5,17 +5,66 @@ export interface Board {
   orientation: string;
   isShipSelected: boolean;
   setIsShipSelected: React.Dispatch<React.SetStateAction<boolean>>;
-  placeShip: (
-    board: number[][],
-    index: number,
-    x: number,
-    y: number
-  ) => number[][] | -1;
-  selectedShip: number;
+  selectedShipIndex: number;
+  isShipPlaced: boolean[];
+  setIsShipPlaced: React.Dispatch<React.SetStateAction<boolean[]>>;
+  shipLengths: number[];
 }
 
 /** Creates the board for placement and for the opponent */
 const Board = (props: Board) => {
+  /**Places down ship on drag ending point respective to the cursor position
+   *
+   * @param index is the index of selected Ship
+   * I am working with index here because there are 2 diff ships with same length of 3
+   * @returns updated board after placement
+   */
+  function placeShip(board: number[][], index: number, x: number, y: number) {
+    if (x === -1 || y === -1) return -1;
+    const xrange = x + props.shipLengths[index];
+    const yrange = y + props.shipLengths[index];
+
+    /** True if neighbouring x or y + ... ship length coordinates is not occupied + !isOutsideGrid */
+    const isLocationPlaceable = (x: number, y: number) => {
+      if (props.orientation === "horizontal") {
+        for (let i = x; i < x + props.shipLengths[index]; i++) {
+          if (board[i][y] !== 0) {
+            return false;
+          }
+        }
+      }
+      if (props.orientation === "vertical") {
+        for (let j = y; j < y + props.shipLengths[index]; j++) {
+          if (board[x][j] !== 0) {
+            return false;
+          }
+        }
+      }
+      return true;
+    };
+
+    const updateShipPlaced = (idx: number | null) => {
+      props.setIsShipPlaced(
+        props.isShipPlaced.map((item, index) => (index === idx ? true : item))
+      );
+    };
+
+    if (props.orientation === "horizontal" && isLocationPlaceable(x, y)) {
+      if (xrange > 10) return -1;
+      for (x; x < xrange; x++) {
+        board[x][y] = props.shipLengths[index];
+        updateShipPlaced(index);
+      }
+    } else if (props.orientation === "vertical" && isLocationPlaceable(x, y)) {
+      if (yrange > 10) return -1;
+      for (y; y < yrange; y++) {
+        board[x][y] = props.shipLengths[index];
+        updateShipPlaced(index);
+      }
+    }
+
+    return board;
+  }
   return (
     <>
       {props.board.map((column, i) => (
@@ -26,7 +75,7 @@ const Board = (props: Board) => {
               id={`row-${j}`}
               onMouseUp={() => {
                 if (!props.isShipSelected) return;
-                props.placeShip(props.board, props.selectedShip, i, j);
+                placeShip(props.board, props.selectedShipIndex, i, j);
                 props.setIsShipSelected(false);
               }}
             >
--- pages/index.tsx
+++ pages/index.tsx
@@ -44,59 +44,6 @@ const Home: NextPage = () => {
       : setOrientation("vertical");
   }
 
-  /**Places down ship on drag ending point respective to the cursor position
-   *
-   * @param index is the index of selected Ship
-   * I am working with index here because there are 2 diff ships with same length of 3
-   * @returns updated board after placement
-   */
-  function placeShip(board: number[][], index: number, x: number, y: number) {
-    if (x === -1 || y === -1) return -1;
-    const xrange = x + shipLengths[index];
-    const yrange = y + shipLengths[index];
-
-    /** True if neighbouring x or y + ... ship length coordinates is not occupied + !isOutsideGrid */
-    const isLocationPlaceable = (x: number, y: number) => {
-      if (orientation === "horizontal") {
-        for (let i = x; i < x + shipLengths[index]; i++) {
-          if (board[i][y] !== 0) {
-            return false;
-          }
-        }
-      }
-      if (orientation === "vertical") {
-        for (let j = y; j < y + shipLengths[index]; j++) {
-          if (board[x][j] !== 0) {
-            return false;
-          }
-        }
-      }
-      return true;
-    };
-
-    const updateShipPlaced = (idx: number | null) => {
-      setIsShipPlaced(
-        isShipPlaced.map((item, index) => (index === idx ? true : item))
-      );
-    };
-
-    if (orientation === "horizontal" && isLocationPlaceable(x, y)) {
-      if (xrange > 10) return -1;
-      for (x; x < xrange; x++) {
-        board[x][y] = shipLengths[index];
-        updateShipPlaced(index);
-      }
-    } else if (orientation === "vertical" && isLocationPlaceable(x, y)) {
-      if (yrange > 10) return -1;
-      for (y; y < yrange; y++) {
-        board[x][y] = shipLengths[index];
-        updateShipPlaced(index);
-      }
-    }
-
-    return board;
-  }
-
   /** Resets the board with zero populated array & places ships randomly on it */
   function handleStart() {
     setOpponentBoard(() => newBoard());
@@ -116,8 +63,10 @@ const Home: NextPage = () => {
             orientation={orientation}
             isShipSelected={isShipSelected}
             setIsShipSelected={setIsShipSelected}
-            placeShip={placeShip}
-            selectedShip={selectedShipIndex}
+            selectedShipIndex={selectedShipIndex}
+            setIsShipPlaced={setIsShipPlaced}
+            isShipPlaced={isShipPlaced}
+            shipLengths={shipLengths}
           ></Board>
         </div>
         {/* grid-value class for computer-side is for debugging (to view values) */}
@@ -127,8 +76,10 @@ const Home: NextPage = () => {
             orientation={orientation}
             isShipSelected={false}
             setIsShipSelected={setIsShipSelected}
-            placeShip={placeShip}
-            selectedShip={selectedShipIndex}
+            selectedShipIndex={selectedShipIndex}
+            setIsShipPlaced={setIsShipPlaced}
+            isShipPlaced={isShipPlaced}
+            shipLengths={shipLengths}
           ></Board>
         </div>
       </div>
