47aaadcc3cb895406e93734a231b300aab2fbba4
SimHoZebs <simhozebs@gmail.com>
2022-11-28 17:56:14 -0500

    update tsc target

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,7 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
