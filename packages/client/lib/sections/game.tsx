3406a51c33553a3d12e242e3f7c3886850a3eac3
ipod1g <kubonkwan99@gmail.com>
2023-03-02 11:08:19 +0800

    Add partial game mechanics

--- packages/client/lib/sections/game.tsx
+++ packages/client/lib/sections/game.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Board from "../components/Board";
-import boardWithRandomlyPlacedShips from "../functions/boardWithRandomlyPlacedShips";
+import boardWithRandomlyPlacedShips from "../functions/generateRandomPlacements";
 import { playerData } from "../interfaces/types";
 // import ServerTest from "../lib/components/ServerTest";
 import Ship from "../components/Ship";
@@ -17,31 +17,122 @@ export interface Props {
 }
 
 const Game = (props: Props) => {
-  const width = 10;
-  const [placementOrientation, setPlacementOrientation] = useState("vertical");
-  const [isShipSelected, setIsShipSelected] = useState(false);
   const [selectedShipIndex, setSelectedShipIndex] = useState<number>(-1);
-  const dragConstraintsRef = useRef(null);
 
-  function newBoard() {
-    return Array(width)
-      .fill(0)
-      .map(() => Array(width).fill(0));
-  }
-  // add updater for board ?
-
-  //stored in server?
   const [userFireLocation, setUserFireLocation] = useState<number[]>([-1, -1]);
   const [userTurn, setUserTurn] = useState(true);
 
+  /** update board array after checking with placed location */
+  function updateHit(
+    location: number[],
+    userData: playerData,
+    setData: React.Dispatch<React.SetStateAction<playerData>>
+  ) {
+    // condition for userturn
+
+    const x = location[0];
+    const y = location[1];
+
+    // If location already shot, alert pick another location
+
+    // const hitStatus = checkHit(location, props.playerData);
+    const { index, shipHit } = checkHit(location, userData);
+    console.log("index of the placedLocation: " + index);
+    console.log("shipType hit: " + shipHit);
+
+    // IF MISS
+    if (index === -1) {
+      //first number as row
+      console.log("miss");
+
+      // Make this piece of code reusable -> updates board and placedlocation
+      setData((prev) => {
+        const updatedBoard = prev.board.map((row, i) => {
+          if (i === x) {
+            return row.map((cell, j) => (j === y ? -1 : cell));
+          }
+          return row;
+        });
+        // const updatedMissedShotsArray = prev.shipInfo.map((ship, i) =>
+        //   i === index
+        //     ? {
+        //         ...ship,
+        //         placedLocation: [...ship.placedLocation, [-1, -1]],
+        //       }
+        //     : ship
+        // );
+        return {
+          ...prev,
+          board: updatedBoard,
+          // shipInfo: updatedMissedShotsArray,
+        };
+      });
+    } else {
+      console.log(props.playerData.shipInfo[shipHit]?.shipType + "-" + index);
+
+      setData((prev) => {
+        const updatedBoard = prev.board.map((row, i) => {
+          if (i === x) {
+            return row.map((cell, j) => (j === y ? -9 : cell));
+          }
+          return row;
+        });
+
+        prev.shipInfo[shipHit].placedLocation[index] = [-x, -y];
+
+        console.log("PLZ: " + JSON.stringify(prev.shipInfo));
+        return { ...prev, board: updatedBoard, shipInfo: prev.shipInfo };
+      });
+    }
+  }
+
+  /** Returns the index of the part of ship hit
+   * This code initializes indexAndShipType with [-1, -1] to represent the case where the location is not found
+   */
+  function checkHit(location: number[], board: playerData) {
+    const indexAndShipType = board.shipInfo.reduce(
+      (result, info, shipType) => {
+        const index = info.placedLocation.findIndex(
+          (locations) => JSON.stringify(locations) === JSON.stringify(location)
+        );
+        if (index >= 0) {
+          result = [index, shipType];
+        }
+        return result;
+      },
+      [-1, -1]
+    );
+
+    // return indexAndShipType;
+    return { index: indexAndShipType[0], shipHit: indexAndShipType[1] };
+  }
+
+  useEffect(() => {
+    console.log(userFireLocation);
+    if (userTurn) {
+      updateHit(userFireLocation, props.opponentData, props.setOpponentData);
+      console.log(userTurn);
+    } else {
+      console.log(userTurn);
+      // updateHit(userFireLocation, props.playerData, props.setPlayerData);
+    }
+    // updateHit(userFireLocation);
+    console.log(props.opponentData);
+
+    return () => {};
+  }, [userFireLocation]);
+
+  useEffect(() => {
+    console.log("playerdata changed");
+
+    return () => {};
+  }, [props.playerData]);
+
   return (
-    <div className="main-container" ref={dragConstraintsRef}>
+    <div className="main-container">
       <div className="game-container">
         <div className="grid-value battleship-grid">
           <Board
-            orientation={placementOrientation}
-            isShipSelected={isShipSelected}
-            setIsShipSelected={setIsShipSelected}
             selectedShipIndex={selectedShipIndex}
             setPlayerData={props.setPlayerData}
             playerData={props.playerData}
@@ -51,9 +142,6 @@ const Game = (props: Props) => {
         </div>
         <div className="grid-value battleship-grid">
           <Board
-            orientation={placementOrientation}
-            isShipSelected={isShipSelected}
-            setIsShipSelected={setIsShipSelected}
             selectedShipIndex={selectedShipIndex}
             setPlayerData={props.setOpponentData}
             playerData={props.opponentData}
